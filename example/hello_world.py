b51f38c3836d1348bfafed0449eea5e976010759
GCBallesteros <dev+github@maxwellrules.com>
2024-06-28 08:45:41 +0200

    feat: Make example with multiple commands

--- example/hello_world.py
+++ example/hello_world.py
@@ -8,5 +8,10 @@ def main(name: str, other, surname: str = "Holy"):
     print(f"Hello {name} {surname}")
 
 
+@app.command()
+def alt(name: str, other: str, surname: str = "Holy", more: str = "more"):
+    print(f"Alt Hello {name} {surname}")
+
+
 if __name__ == "__main__":
     app()
