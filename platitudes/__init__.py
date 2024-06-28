20e4051de1a0c98bb484edb7827ed3525f869b18
GCBallesteros <dev+github@maxwellrules.com>
2024-06-28 11:34:40 +0200

    feat: Add support for env variables

--- platitudes/__init__.py
+++ platitudes/__init__.py
@@ -1,7 +1,8 @@
-__version__ = "0.0.0"
+__version__ = "0.1.0"
 
 import argparse
 import inspect
+import os
 import sys
 from typing import Annotated, Callable, get_args, get_origin
 
@@ -29,12 +30,13 @@ class Platitudes:
             cmd_signature = inspect.signature(function)
 
             for param_name, param in cmd_signature.parameters.items():
-                # Set default values for the arguments parameters which may
-                # be overriden.
+                # Set default values for the arguments parameters which may be
+                # overriden.
                 help = None
                 type_ = str
                 default = None
                 optional_prefix = ""
+                envvar = None
 
                 if (annot := param.annotation) is not inspect._empty:
                     type_ = annot
@@ -44,11 +46,27 @@ class Platitudes:
                         for arg in annot_args:
                             if isinstance(arg, Argument):
                                 help = arg.help
+                                envvar = arg.envvar
                                 break
 
                 if _has_default_value(param):
                     default = param.default
                     optional_prefix = "--"
+
+                    # If we are using an envvar we use it only if it is available on
+                    # the environemnt otherwise don't do anything, ie. keep the default
+                    if envvar is not None:
+                        try:
+                            default = os.environ[envvar]
+                        except KeyError:
+                            pass
+
+                else:
+                    if envvar is not None:
+                        e_ = (
+                            "Envvars are not supported for arguments without a default."
+                        )
+                        raise ValueError(e_)
 
                 cmd_parser.add_argument(
                     f"{optional_prefix}{param_name}",
