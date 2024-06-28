6c7725f94884f8188af1e1fb0e5e6e95c3a41d9b
GCBallesteros <dev+github@maxwellrules.com>
2024-06-28 08:49:36 +0200

    refactor: Rename variables
    Some have been made private

--- platitudes/__init__.py
+++ platitudes/__init__.py
@@ -9,11 +9,11 @@ from typing import Callable
 class Platitudes:
     def __init__(self):
         self._registered_commands: dict[str, Callable] = {}
-        self.parser = argparse.ArgumentParser()
-        self.subparsers = self.parser.add_subparsers()
+        self._parser = argparse.ArgumentParser()
+        self._subparsers = self._parser.add_subparsers()
 
     def __call__(self) -> None:
-        args_ = self.parser.parse_args()
+        args_ = self._parser.parse_args()
         main_command = self._registered_commands[sys.argv[1]]
 
         args_dict = dict(args_._get_kwargs())
@@ -21,16 +21,16 @@ class Platitudes:
 
     def command(self):
         def f(function: Callable) -> Callable:
-            command_parser = self.subparsers.add_parser(function.__name__)
-            command_signature = inspect.signature(function)
+            cmd_parser = self._subparsers.add_parser(function.__name__)
+            cmd_signature = inspect.signature(function)
 
-            for param_name, param in command_signature.parameters.items():
+            for param_name, param in cmd_signature.parameters.items():
                 if (annot := param.annotation) is inspect._empty:
                     type_ = str
                 else:
                     type_ = annot
 
-                command_parser.add_argument(param_name, type=type_)
+                cmd_parser.add_argument(param_name, type=type_)
 
                 self._registered_commands[function.__name__] = function
 
